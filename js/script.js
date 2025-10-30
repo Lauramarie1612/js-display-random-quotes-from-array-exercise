5a62d68c20a10e08e24c84ea215175270556d50d
Lauramarie1612 <eaa25lmbj@students.eaaa.dk>
2025-10-30 21:26:58 +0100

    nyeste forsøg

--- js/script.js
+++ js/script.js
@@ -1,57 +1,54 @@
 "use strict";
 
-//Objekter i tuborgklammerne, og den skarpe klamme indikerer at vi gerne vil have en array/liste. Author og quote er proporties og navnet og quotesne er values. Values inde i "" er strings.
-
+// Arrays med citater
 const quotesListe = [
-  { author: "René Descartes", quote: "I think, therefore I am." },
   {
-    author: "William Shakespeare",
-    quote: "To be, or not to be: that is the question.",
-  },
-  {
-    author: "Franklin D. Roosevelt",
-    quote: "The only thing we have to fear is fear itself.",
-  },
-  {
-    author: "Mark Twain",
+    author: "Albert Einstein",
     quote:
-      "The two most important days in your life are the day you are born... and the day you find out why.",
+      "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+  },
+  {
+    author: "Maya Angelou",
+    quote:
+      "You will face many defeats in life, but never let yourself be defeated.",
+  },
+  {
+    author: "Oscar Wilde",
+    quote: "Be yourself; everyone else is already taken.",
+  },
+  {
+    author: "Nelson Mandela",
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  },
+  {
+    author: "Steve Jobs",
+    quote: "Innovation distinguishes between a leader and a follower.",
   },
 ];
 
-//Funktion uden parameter
+// Funktion til at få et tilfældigt citat
 function getRandomQuote() {
-  //Tjekker om listen er tom, og laver en fejlbesked hvis den er det
   if (quotesListe.length === 0) {
     const errorMsg = "No quotes available!!!";
-    document.getElementById("quotes").innerHTML = <p>${errorMsg}</p>;
+    document.getElementById("quotes").innerHTML = `<p>${errorMsg}</p>`;
     return;
   }
 
-  // Hvis der ER quotes, vælg et tilfældigt index
   const randomIndex = Math.floor(Math.random() * quotesListe.length);
-
-  // Returner quote-objektet fra arrayet
   return quotesListe[randomIndex];
 }
 
-//Ny funktion uden parameter
 function displayRandomQuote() {
   const randomQuote = getRandomQuote();
-
-  if (!randomQuote) return;
-
-  // Opretter HTML struktur til quote + author
   let quoteDetails = "";
-  quoteDetails += `
-        <p>${randomQuote.quote}</p>
-        <h4>${randomQuote.author}</h4>
-    `;
 
-  // Finder <p id="quotes"> og sætter indholdet
+  if (randomQuote) {
+    quoteDetails = `
+      <p>"${randomQuote.quote}"</p>
+      <p><em>- ${randomQuote.author}</em></p>
+    `;
+  }
+
   document.getElementById("quotes").innerHTML = quoteDetails;
 }
-
-//Tilføjer event til knappen
-const displayQuoteBtn = document.getElementById("displayQuoteBtn");
-displayQuoteBtn.addEventListener("click", displayRandomQuote);
