9fa63fbee7a8ea6b6217369dae83430272d3ed21
Lauramarie1612 <eaa25lmbj@students.eaaa.dk>
2025-10-30 21:35:06 +0100

    nu

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 "use strict";
 
-// Arrays med citater
+// Array of quotes
 const quotesListe = [
   {
     author: "Albert Einstein",
     quote:
       "Life is like riding a bicycle. To keep your balance, you must keep moving.",
   },
+
   {
     author: "Maya Angelou",
     quote:
       "You will face many defeats in life, but never let yourself be defeated.",
   },
+
   {
     author: "Oscar Wilde",
     quote: "Be yourself; everyone else is already taken.",
   },
+
   {
     author: "Nelson Mandela",
     quote:
       "The greatest glory in living lies not in never falling, but in rising every time we fall.",
   },
+
   {
     author: "Steve Jobs",
     quote: "Innovation distinguishes between a leader and a follower.",
   },
 ];
 
-// Funktion til at få et tilfældigt citat
+// Function to get a random quote
 function getRandomQuote() {
   if (quotesListe.length === 0) {
     const errorMsg = "No quotes available!!!";
@@ -39,6 +43,7 @@ function getRandomQuote() {
   return quotesListe[randomIndex];
 }
 
+// Function to display a random quote
 function displayRandomQuote() {
   const randomQuote = getRandomQuote();
   let quoteDetails = "";
@@ -50,5 +55,11 @@ function displayRandomQuote() {
     `;
   }
 
-  document.getElementById("quotes").innerHTML = quoteDetails;
+  // Find the element and set innerHTML
+  const quotesElement = document.getElementById("quotes");
+  quotesElement.innerHTML = quoteDetails;
 }
+
+// Add event listener to the button
+const displayQuoteBtn = document.getElementById("displayQuoteBtn");
+displayQuoteBtn.addEventListener("click", displayRandomQuote);
